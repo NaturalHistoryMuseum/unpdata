55283b73a859b16616f8b45c9811515a1d3611ac
Ed Baker <edwbaker@gmail.com>
2022-07-13 15:28:41 +0100

    Initia commit

---
+++ R/sites.R
@@ -0,0 +1,25 @@
+sites <- function() {
+  names <- c(
+    "Natural History Museum",
+    "Great North Museum",
+    "Kelvingrove",
+    "National Museum Wales",
+    "Ulster Museum"
+  )
+  lat <- c(
+    51.496111,
+    54.98,
+    55.868581,
+    51.4858,
+    54.582
+  )
+  lon <- c(
+    -0.176389,
+    -1.613,
+    -4.290506,
+    -3.1773,
+    -5.935
+  )
+  ret <- as.data.frame(cbind(names,lat,lon))
+  return(ret)
+}
