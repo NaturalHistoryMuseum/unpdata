6a94e0e07bf1a10cb0a7063bd647a97f598127fc
Ed Baker <929684+edwbaker@users.noreply.github.com>
2022-11-24 14:57:26 +0000

    Sampling sites

---
+++ R/sample_Sites.R
@@ -0,0 +1,25 @@
+#' Sample sites data for Urban Nature Project
+#'
+#' Provides a data frame of information regarding the UNP sampling sites.
+#'
+#' @export
+sample_sites <- function() {
+  code <- c(
+    "Glasgow1",
+    "Glasgow2",
+    "Newcastle1",
+    "Newcastle2"
+  )
+  name <- c(
+    "Springburn Allotments",
+    "North Kelvin Meadow",
+    "Gosforth Nature Reserve",
+    "Gosforth Nature Reserve Visitor Center"
+  )
+  OSgrid <- c(
+    "NS60236880",
+    "NS57016787",
+    "NZ25666996",
+    "NZ25966989"
+  )
+}
