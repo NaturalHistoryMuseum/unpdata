45a447007ffe936c7941a634132c8efbe1c06c39
Ed Baker <edwbaker@gmail.com>
2023-12-19 14:08:00 +0000

    Bristol Community Gardens deploymnets

--- R/sample_Sites.R
+++ R/sample_Sites.R
@@ -9,19 +9,29 @@ sample_sites <- function() {
     "Glasgow-1",
     "Glasgow-2",
     "Newcastle-1",
-    "Newcastle-2"
+    "Newcastle-2",
+    "device-1-pond",
+    "device-2-orchard",
+    "device-3-church"
   )
   name <- c(
     "Springburn Allotments",
     "North Kelvin Meadow",
     "Gosforth Nature Reserve",
-    "Gosforth Nature Reserve Visitor Center"
+    "Gosforth Nature Reserve Visitor Center",
+    "Bristol Community Gardens - Pond",
+    "Bristol Community Gardens - Orchard",
+    "Bristol Community Gardens - Fence near Church"
   )
   OSgrid <- c(
     "NS60236880",
     "NS57016787",
     "NZ25666996",
-    "NZ25966989"
+    "NZ25966989",
+    "ST58976767",
+    "ST58926767",
+    "ST59006756"
+
   )
 
   wgs84 <- osg_parse(OSgrid, coord_system = "WGS84")
